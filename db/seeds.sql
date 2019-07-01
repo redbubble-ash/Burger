9f95af820ebef09267457cfc783141cf64a3fc3e
Your Name <you@example.com>
2019-07-01 00:39:43 -0700

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUE ("Angus burger");
+INSERT INTO burgers (burger_name) VALUE ("Bacon cheeseburger");
+INSERT INTO burgers (burger_name) VALUE ("Aussie burger");
+INSERT INTO burgers (burger_name) VALUE ("Barbecue burger");
+INSERT INTO burgers (burger_name) VALUE ("Buffalo burger");
