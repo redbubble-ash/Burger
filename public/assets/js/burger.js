bac4a5c21983d159660c61f618df90d3bf44fc76
Your Name <you@example.com>
2019-07-02 00:20:26 -0700

    partials

--- public/assets/js/burger.js
+++ public/assets/js/burger.js
@@ -23,11 +23,31 @@ $(".btn btn-success").on("click",function(event){
             }
           );
       
-
+       
 
 })
 
 
+$(".change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newDevour");
+
+    var newDevourState = {
+        devoured: newDevour
+    };
+
+    // Send the PUT request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(
+      function() {
+        console.log("changed devoured to", newDevour);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 
 
 
