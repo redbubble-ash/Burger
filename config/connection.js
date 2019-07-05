7106fb8093609b3adc96ffc7526bca935a1c3fe1
Your Name <you@example.com>
2019-07-05 12:02:17 -0700

    JawsDB connection

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,19 @@
 var mysql = require("mysql");
 require("dotenv").config();
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE_NAME
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE_NAME
+// });
 
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
+  port: 3306,
   host: 'localhost',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,25 @@
-DROP DATABASE IF EXISTS burgers_db;
+-- DROP DATABASE IF EXISTS burgers_db;
 
-CREATE DATABASE burgers_db;
-USE burgers_db;
+-- CREATE DATABASE burgers_db;
+-- USE burgers_db;
+
+-- CREATE TABLE burgers
+-- (
+--     id int NOT NULL AUTO_INCREMENT,
+--     burger_name varchar(255) NOT NULL,
+--     devoured BOOLEAN DEFAULT false,
+--     PRIMARY KEY (id)
+-- );
+
+USE cvr68yclhou8kki9;
 
 CREATE TABLE burgers
 (
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
+    createAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
+
+SELECT * FROM cvr68yclhou8kki9.burgers;
