30eb8c49a1d35279dff3a43b95445f30e1ddf586
Your Name <you@example.com>
2019-07-05 12:17:02 -0700

    sql update

--- db/schema.sql
+++ db/schema.sql
@@ -18,7 +18,6 @@ CREATE TABLE burgers
     id int NOT NULL AUTO_INCREMENT,
     burger_name varchar(255) NOT NULL,
     devoured BOOLEAN DEFAULT false,
-    createAt TIMESTAMP NOT NULL,
     PRIMARY KEY (id)
 );
 
